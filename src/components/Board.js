d3e53f58be5382f226f74598fc43948ddee37484
onurkaymak <onurkaymak@outlook.com>
2023-11-29 09:54:46 -0800

    Create Square component, import Square into Board component.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,25 @@
-export default function Board() {
+import Square from "./Square";
+
+const Board = () => {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
+
+export default Board;
---
+++ src/components/Square.js
@@ -0,0 +1,6 @@
+const Square = (props) => {
+
+  return <button className="square">{props.value}</button>
+};
+
+export default Square;
