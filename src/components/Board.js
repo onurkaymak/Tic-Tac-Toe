faad50c50f304cdf72d4ad56ad7bd76f3de9281a
onurkaymak <onurkaymak@outlook.com>
2023-11-29 10:53:47 -0800

    Create handleClick method, pass handleClick method to Square component.

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,22 +5,28 @@ const Board = () => {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const handleClick = () => {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares((nextSquares));
+  };
+
   return (
     <div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </div>
   );
