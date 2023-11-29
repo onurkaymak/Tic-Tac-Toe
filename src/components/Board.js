625697fc8da7003b5a6ba3e4e074428efc3a8fc0
onurkaymak <onurkaymak@outlook.com>
2023-11-29 11:45:06 -0800

    Add branching to avoid marking same square twice.

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,6 +8,10 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i) => {
+    if (squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
