cf14cdec2b06ce4abf01a578dfdfe8f640304a63
onurkaymak <onurkaymak@outlook.com>
2023-11-29 13:45:36 -0800

    Add branching for winner UI.

--- src/components/Board.js
+++ src/components/Board.js
@@ -43,8 +43,19 @@ const Board = () => {
     return null;
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+
+
   return (
     <div>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => { handleClick(0) }} />
         <Square value={squares[1]} onSquareClick={() => { handleClick(1) }} />
