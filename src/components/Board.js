6f8b450cd2dc5ab30b90dce20b06d84e1fd60987
onurkaymak <onurkaymak@outlook.com>
2023-11-29 11:46:23 -0800

    Create calculateWinner method.

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,7 +8,7 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i) => {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -21,6 +21,27 @@ const Board = () => {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   };
+
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
   return (
     <div>
